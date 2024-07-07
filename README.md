c1e88e94fab94561e225a4ffefe7a86aedf77243
Dikshant Sharma <sdikshant89@gmail.com>
2024-07-06 20:23:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lockbase_service
+ 
---
+++ src/main/java/com/lockbase/application/LockbaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lockbase.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockbaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockbaseServiceApplication.class, args);
+	}
+
+}
